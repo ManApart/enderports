3506c1432ddaffbd1c6a2084796dc44ec68e482c
ManApart <raustinkucera@gmail.com>
2019-09-16 17:49:57 -0400

    recipe, loot table, breakage, textures

--- src/main/java/org/manapart/enderports/Teleporter.java
+++ src/main/java/org/manapart/enderports/Teleporter.java
@@ -3,25 +3,43 @@ package org.manapart.enderports;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SlabBlock;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.ServerWorld;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ToolType;
 
 import javax.annotation.Nullable;
 
 public class Teleporter extends SlabBlock {
 
     public Teleporter() {
-        super(Block.Properties.create(Material.IRON, MaterialColor.BLUE));
+        super(Block.Properties.create(Material.IRON, MaterialColor.BLUE).hardnessAndResistance(4f).sound(SoundType.METAL));
+    }
+
+    @Nullable
+    @Override
+    public ToolType getHarvestTool(BlockState state) {
+        return ToolType.PICKAXE;
+    }
+
+    @Override
+    public int getHarvestLevel(BlockState state) {
+        return 2; //Harvest level of 2 means it requires iron or better to harvest
+    }
+
+    @Override
+    public ResourceLocation getLootTable() {
+        return super.getLootTable();
     }
 
     @Override
@@ -29,12 +47,10 @@ public class Teleporter extends SlabBlock {
         boolean result = super.onBlockActivated(state, world, pos, player, hand, rayTraceResult);
         System.out.println("On block clicked");
         if (world instanceof ServerWorld) {
-//        if (world instanceof ServerWorld && Minecraft.getInstance().player.getEntityId() == player.getEntityId()) {
             ServerWorld serverWorld = (ServerWorld) world;
             BlockPos nextPos = TeleporterNetwork.getNetwork(serverWorld).getNextTeleporter(pos);
             if (!pos.equals(nextPos)) {
                 ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
-//                Minecraft.getInstance().player.setPosition(nextPos.getX() + .5, nextPos.getY() + 1, nextPos.getZ() + .5);
                 serverPlayer.connection.setPlayerLocation(nextPos.getX() + .5, nextPos.getY() + 1, nextPos.getZ() + .5, 0, 0);
             }
         }
