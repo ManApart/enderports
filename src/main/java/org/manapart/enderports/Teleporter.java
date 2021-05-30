d5c1a0666c5e840afc23665cd3ebbf666fcdbc9a
ManApart <r@austinkucera.com>
2021-05-30 18:09:57 -0400

    attempt alternate methods

--- src/main/java/org/manapart/enderports/Teleporter.java
+++ src/main/java/org/manapart/enderports/Teleporter.java
@@ -10,6 +10,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.ToolType;
@@ -32,38 +33,36 @@ public class Teleporter extends SlabBlock {
         return props;
     }
 
+    @Override
+    public ActionResultType use(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult rayTraceResult) {
+        if (world instanceof ServerWorld) {
+            ServerWorld serverWorld = (ServerWorld) world;
+            BlockPos nextPos = TeleporterNetwork.getNetwork(serverWorld).getNextTeleporter(pos);
+            if (!pos.equals(nextPos)) {
+                ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
+                serverPlayer.connection.teleport(nextPos.getX() + .5, nextPos.getY() + 1, nextPos.getZ() + .5, serverPlayer.yHeadRot, 0);
+                serverWorld.playSound(null, nextPos, SoundEvents.ENDERMAN_TELEPORT, SoundCategory.PLAYERS, 1f, 1f );
+            }
+        }
 
+        return ActionResultType.PASS;
+    }
 
-//    @Override
-//    public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult rayTraceResult) {
-//        if (world instanceof ServerWorld) {
-//            ServerWorld serverWorld = (ServerWorld) world;
-//            BlockPos nextPos = TeleporterNetwork.getNetwork(serverWorld).getNextTeleporter(pos);
-//            if (!pos.equals(nextPos)) {
-//                ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
-//                serverPlayer.connection.setPlayerLocation(nextPos.getX() + .5, nextPos.getY() + 1, nextPos.getZ() + .5, serverPlayer.getYaw(0f), 0);
-//                serverWorld.playSound(null, nextPos, SoundEvents.ENTITY_ENDERMAN_TELEPORT, SoundCategory.PLAYERS, 1f, 1f );
-//            }
-//        }
-//
-//        return ActionResultType.PASS;
-//    }
-//
-//    @Override
-//    public void onBlockPlacedBy(World world, BlockPos blockPos, BlockState state, @Nullable LivingEntity entity, ItemStack itemStack) {
-//        super.onBlockPlacedBy(world, blockPos, state, entity, itemStack);
-//        if (world instanceof ServerWorld) {
-//            TeleporterNetwork network = TeleporterNetwork.getNetwork((ServerWorld) world);
-//            network.reBalance();
-//            network.addTeleporter(blockPos);
-//        }
-//    }
-//
-//    @Override
-//    public void onBlockHarvested(World world, BlockPos blockPos, BlockState blockState, PlayerEntity playerEntity) {
-//        super.onBlockHarvested(world, blockPos, blockState, playerEntity);
-//        if (world instanceof ServerWorld) {
-//            TeleporterNetwork.getNetwork((ServerWorld) world).removeTeleporter(blockPos);
-//        }
-//    }
+    @Override
+    public void onPlace(BlockState state, World world, BlockPos pos, BlockState state2, boolean boolThing) {
+        super.onPlace(state, world, pos, state2, boolThing);
+        if (world instanceof ServerWorld) {
+            TeleporterNetwork network = TeleporterNetwork.getNetwork((ServerWorld) world);
+            network.reBalance();
+            network.addTeleporter(pos);
+        }
+    }
+
+    @Override
+    public void onBlockExploded(BlockState state, World world, BlockPos pos, Explosion explosion) {
+        super.onBlockExploded(state, world, pos, explosion);
+        if (world instanceof ServerWorld) {
+            TeleporterNetwork.getNetwork((ServerWorld) world).removeTeleporter(pos);
+        }
+    }
 }
