6bd3b892d42e6221a4ce1f0e5f4b278acf413840
ManApart <r@austinkucera.com>
2024-05-11 18:51:31 -0400

    add to item groups

--- src/main/java/org/manapart/enderports/EnderPorts.kt
+++ src/main/java/org/manapart/enderports/EnderPorts.kt
@@ -1,7 +1,12 @@
 package org.manapart.enderports
 
+import net.minecraft.world.item.CreativeModeTabs
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent
 import net.minecraftforge.fml.common.Mod
+import org.manapart.enderports.ModBlocks.TELEPORTER_BLOCK
+import org.manapart.enderports.ModItems.TELEPORT_ITEM
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
+
 
 const val MODID = "enderports"
 
@@ -12,6 +17,14 @@ object EnderPorts {
         ModBlocks.REGISTRY.register(MOD_BUS)
         ModItems.REGISTRY.register(MOD_BUS)
         ModEntities.REGISTRY.register(MOD_BUS)
+        MOD_BUS.addListener(::buildContents)
+    }
+
+    fun buildContents(event: BuildCreativeModeTabContentsEvent) {
+        if (event.tabKey == CreativeModeTabs.REDSTONE_BLOCKS) {
+            event.accept(TELEPORT_ITEM)
+            event.accept(TELEPORTER_BLOCK)
+        }
     }
 
 }
