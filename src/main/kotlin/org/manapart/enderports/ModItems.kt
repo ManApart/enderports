5c62bca3af7e956e48620949daf5c1e4bcc37404
ManApart <r@austinkucera.com>
2026-01-20 17:13:48 -0500

    place block

--- src/main/kotlin/org/manapart/enderports/ModItems.kt
+++ src/main/kotlin/org/manapart/enderports/ModItems.kt
@@ -5,13 +5,13 @@ import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.core.registries.Registries
 import net.minecraft.resources.Identifier
 import net.minecraft.resources.ResourceKey
+import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.Item
 
 object ModItems {
     fun initialize() {}
     val TELEPORT_ITEM = register(teleporterBlockId) {
-//        TeleporterItem(ModBlocks.TELEPORTER_BLOCK)
-        Item(it)
+        BlockItem(ModBlocks.TELEPORTER_BLOCK, it)
     }
 
     fun <GenericItem : Item> register(name: String, settings: Item.Properties = Item.Properties(), itemFactory: (Item.Properties) -> GenericItem): GenericItem {
--- src/main/kotlin/org/manapart/enderports/TeleporterItem.kt
+++
@@ -1,7 +0,0 @@
-package org.manapart.enderports
-
-import net.minecraft.world.item.BlockItem
-import net.minecraft.world.item.CreativeModeTab
-
-
-//class TeleporterItem(block: Teleporter) : BlockItem(block, Properties())
